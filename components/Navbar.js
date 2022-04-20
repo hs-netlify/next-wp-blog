a6ad9e3b93997d6c30e90e3ba702db6baf9ca749
Henry Smith <henry.smith@netlify.com>
2022-04-20 12:07:33 +0100

    Add tailwind + clean nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,18 @@
 import Link from "next/link";
 
 export default function Navbar() {
+  const navLink = `hover:text-red-600 hover:underline`;
+
   return (
-    <div className="container">
+    <div className="p-2">
       <nav>
         <ul>
-          <li>
+          <li className={navLink}>
             <Link href="/">
               <a>Home</a>
             </Link>
           </li>
-          <li>
+          <li className={navLink}>
             <Link href="/contact">
               <a>Contact Us</a>
             </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,58 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { gql } from '@apollo/client';
+import Head from "next/head";
+import Link from "next/link";
+import { gql } from "@apollo/client";
 
-import { getApolloClient } from 'lib/apollo-client';
-
-import styles from '../styles/Home.module.css'
+import { getApolloClient } from "lib/apollo-client";
 
 export default function Home({ page, posts }) {
   const { title, description } = page;
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>{title}</h1>
+      <main className="main">
+        <h1 className="title text-red-400">{title}</h1>
 
-        <p className={styles.description}>{ description }</p>
+        <p className="description">{description}</p>
 
-        <ul className={styles.grid}>
-          {posts && posts.length > 0 && posts.map(post => {
-            return (
-              <li key={post.slug} className={styles.card}>
-                <Link href={post.path}>
-                  <a>
-                    <h3 dangerouslySetInnerHTML={{
-                      __html: post.title
-                    }} />
-                    <div dangerouslySetInnerHTML={{
-                      __html: post.excerpt
-                    }} />
-                  </a>
-                </Link>
+        <ul className="grid">
+          {posts &&
+            posts.length > 0 &&
+            posts.map((post) => {
+              return (
+                <li key={post.slug} className="card">
+                  <Link href={post.path}>
+                    <a>
+                      <h3
+                        dangerouslySetInnerHTML={{
+                          __html: post.title,
+                        }}
+                      />
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: post.excerpt,
+                        }}
+                      />
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+
+          {!posts ||
+            (posts.length === 0 && (
+              <li>
+                <p>Oops, no posts found!</p>
               </li>
-            );
-          })}
-
-          {!posts || posts.length === 0 && (
-            <li>
-              <p>
-                Oops, no posts found!
-              </p>
-            </li>
-          )}
+            ))}
         </ul>
       </main>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
@@ -76,21 +79,23 @@ export async function getStaticProps() {
     `,
   });
 
-  const posts = data?.data.posts.edges.map(({ node }) => node).map(post => {
-    return {
-      ...post,
-      path: `/posts/${post.slug}`
-    }
-  });
+  const posts = data?.data.posts.edges
+    .map(({ node }) => node)
+    .map((post) => {
+      return {
+        ...post,
+        path: `/posts/${post.slug}`,
+      };
+    });
 
   const page = {
-    ...data?.data.generalSettings
-  }
+    ...data?.data.generalSettings,
+  };
 
   return {
     props: {
       page,
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
