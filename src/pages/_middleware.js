e14176c80ca4bfba6697aeace0d97578b63bcd4b
Henry Smith <henry.smith@netlify.com>
2022-04-21 16:21:20 +0100

    Update middleware

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -3,7 +3,6 @@ export function middleware(req) {
   const someCookie = req.cookies["something"];
   const base = process.env.NEXT_PUBLIC_BASE_URL;
 
-  console.log(someCookie);
   const res = someCookie ? NextResponse.rewrite(`${base}/${someCookie}`) : null;
 
   return res;
