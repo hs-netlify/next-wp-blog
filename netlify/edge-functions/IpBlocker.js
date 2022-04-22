109c64faee02dcdb25e9af845ee983f360e082ee
Henry Smith <henry.smith@netlify.com>
2022-04-22 11:29:14 +0100

    Header check

--- netlify/edge-functions/IpBlocker.js
+++ netlify/edge-functions/IpBlocker.js
@@ -3,20 +3,21 @@ import ipRangeCheck from "https://dev.jspm.io/ip-range-check";
 export default async (req, context) => {
   //const whitelist = JSON.parse(process.env.WHITE_LIST);
   const whitelist = ["128.0.0.0/1"];
-  const ip = req.headers.get("x-forwarded-for");
-  console.log(ip);
-  // const ip = "192.168.1.1";
-  if (ipRangeCheck(ip, whitelist)) {
-    console.log("here");
-    // const random = Math.random();
-    // const random = 0.1;
+  let headers = [];
+  for (var pair of req.headers.entries()) {
+    headers.push(pair);
+  }
 
-    // if (random > 0.5) {
+  console.log(headers);
+
+  const ip = req.headers.get("x-forwarded-for");
+
+  if (ipRangeCheck(ip, whitelist)) {
     return new Response(`Your ip is ${ip}`);
   } else {
     return new Response(
       `We're sorry, you can't access our content! - Your req ${JSON.stringify(
-        req
+        headers
       )}`,
       {
         headers: { "content-type": "text/html" },
