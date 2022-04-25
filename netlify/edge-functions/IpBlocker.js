eccca8a92c0ed4c8db0d0431874fe9217ab4e68f
Henry Smith <henry.smith@netlify.com>
2022-04-25 16:34:15 +0100

    Update allow list params

--- netlify/edge-functions/IpBlocker.js
+++ netlify/edge-functions/IpBlocker.js
@@ -1,18 +1,18 @@
 import ipRangeCheck from "https://dev.jspm.io/ip-range-check";
 
 export default async (req, context) => {
-  //const whitelist = JSON.parse(process.env.WHITE_LIST);
-  const whitelist = ["128.0.0.0/1"];
-  let headers = [];
-  for (var pair of req.headers.entries()) {
-    headers.push(pair);
-  }
-  console.log(req.headers);
-  console.log(JSON.stringify(context));
+  //const allowList = JSON.parse(process.env.ALLOW_LIST);
+  // const allowList=["128.0.0.0/1"];
+  // let headers = [];
+  // for (var pair of req.headers.entries()) {
+  //   headers.push(pair);
+  // }
+  // console.log(req.headers.get("x-nf-client-connection-ip"));
+  // console.log(JSON.stringify(context));
 
   const ip = req.headers.get("x-forwarded-for");
 
-  if (ipRangeCheck(ip, whitelist)) {
+  if (ipRangeCheck(ip, allowList)) {
     return new Response(`Your ip is ${ip}`);
   } else {
     return new Response(
--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 export function middleware(req) {
+  console.log("this middleware req", req);
   const someCookie = req.cookies["something"];
   const base = process.env.NEXT_PUBLIC_BASE_URL;
 
